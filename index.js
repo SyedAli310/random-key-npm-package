7187d1c0aabd03d5857dca67735bed4778ec1912
Syed Ali <syed.a@keka.com>
2023-08-01 18:37:04 +0530

    added key generation with a pattern

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-module.exports = generateKey = (tokenLen, hyphen, hyphenPos) => {
-    let keySet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    var token = [];
-    if (hyphen == true) {
-      for (let i = 0; i < tokenLen; i++) {
-        let pos = Math.floor(Math.random() * keySet.length);
-        if (hyphenPos == undefined) hyphenPos = 1;
-        if (i % hyphenPos == 0) {
-          token.push("-");
-        }
-        token.push(keySet[pos]);
+const generateKey = (tokenLen, hyphen, hyphenPos) => {
+  let keySet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var token = [];
+  if (hyphen == true) {
+    for (let i = 0; i < tokenLen; i++) {
+      let pos = Math.floor(Math.random() * keySet.length);
+      if (hyphenPos == undefined) hyphenPos = 1;
+      if (i % hyphenPos == 0) {
+        token.push("-");
       }
-      token.shift();
-    } else if (hyphen == false) {
-      for (let i = 0; i < tokenLen; i++) {
-        let pos = Math.floor(Math.random() * keySet.length);
-        token.push(keySet[pos]);
-      }
-    } else {
-      token = ["Incorrect", " ", "Arguments", " ", "Provided"];
+      token.push(keySet[pos]);
     }
-    //console.log(token);
-    token = token.join("");
-    return token;
-  };  
+    token.shift();
+  } else if (hyphen == false) {
+    for (let i = 0; i < tokenLen; i++) {
+      let pos = Math.floor(Math.random() * keySet.length);
+      token.push(keySet[pos]);
+    }
+  } else {
+    return new Error('Invalid arguments');
+  }
+  //console.log(token);
+  token = token.join("");
+  return token;
+};
+
+const generateKeyPattern = (pattern) => {
+  const hexChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let guid = '';
+  if(!pattern || !pattern.length) return new Error('Please provide pattern (eg: xxx-xxxx-xxx)');
+  for (let i = 0; i < pattern.length; i++) {
+    if (pattern[i] === 'x') {
+      const randomIndex = Math.floor(Math.random() * 16);
+      guid += hexChars[randomIndex];
+    } else {
+      guid += pattern[i];
+    }
+  }
+  return guid;
+}
+
+module.exports = {
+  generateKey,
+  generateKeyPattern
+}
